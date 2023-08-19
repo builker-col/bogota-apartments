dbca78c987d7943bfc0df4c5f1c519d53f2a91c6
zer0 <erik172dev@gmail.com>
2023-08-19 16:38:34 -0500

    RAW DATA V1.2.0 AUGUST.1

--- data/raw/download_raw_data.py
+++ data/raw/download_raw_data.py
@@ -1,7 +1,8 @@
 import requests
 
 # Obtenga la URL del archivo JSON
-url = "https://www.dropbox.com/s/1ly47276dnqqdzp/builker.scrapy_bogota_apartments.json?dl=1"
+# url = "https://www.dropbox.com/s/1ly47276dnqqdzp/builker.scrapy_bogota_apartments.json?dl=1"
+url = "https://www.dropbox.com/scl/fi/bxj03wii0ez50ixe9q5yv/builker.scrapy_bogota_apartments.json?rlkey=btg69ut2biha7xd1j5llk0gj4&dl=1"
 
 # Haga una solicitud GET a la URL
 response = requests.get(url)
