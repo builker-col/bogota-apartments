de865f37b3c03212212d2527d7a12795b087de9a
zer0 <erik172dev@gmail.com>
2023-09-06 15:44:19 -0500

    v1.2.1 METROCUADRADO SCRAPER

--- bogota_apartments/spiders/metrocuadrado_spider.py
+++ bogota_apartments/spiders/metrocuadrado_spider.py
@@ -24,7 +24,7 @@ class MetrocuadradoSpider(scrapy.Spider):
         Initializes the spider with a headless Chrome browser instance
         """
         chrome_options = Options()
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--window-size=1920x1080')
         chrome_options.add_argument(f'user-agent={UserAgent().random}')
 
