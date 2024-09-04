1c378c419945acda71bcabdc505725ffa0bc7762
erik <erik172dev@gmail.com>
2024-09-04 09:04:32 -0500

    chore: Update TransMilenio stations data source URL in ETL script

--- ETL/03_data_enrichment.py
+++ ETL/03_data_enrichment.py
@@ -82,7 +82,7 @@ apartments = pd.read_csv('data/interim/apartments.csv', low_memory=False)
 
 # Get TransMilenio stations data
 logging.info('Getting TransMilenio stations data...')
-response = requests.get('https://gis.transmilenio.gov.co/arcgis/rest/services/Troncal/consulta_estaciones_troncales/FeatureServer/1/query?where=1%3D1&outFields=*&f=json').json()
+response = requests.get('https://gis.transmilenio.gov.co/arcgis/rest/services/Troncal/consulta_estaciones_troncales/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json').json()
 troncal_transmilenio = pd.DataFrame(response['features'])
 troncal_transmilenio = pd.json_normalize(troncal_transmilenio['attributes'])
 
